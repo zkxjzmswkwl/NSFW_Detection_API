2b40113674f0de57b5e439df25281080d352488a
zkxjzmswkwl <smithcarterfreelance@gmail.com>
2022-06-06 16:58:33 -0700

    Resize image to 224x224, as the model requires.

--- api/functions.py
+++ api/functions.py
@@ -2,6 +2,7 @@ from config import MAX_IMAGE_SIZE
 from random import randint
 import aiohttp
 import aiofiles
+from PIL import Image
 
 MAX_IMAGE_SIZE = MAX_IMAGE_SIZE * 1000000
 
@@ -10,12 +11,16 @@ async def download_image(url):
     file_name = f"{randint(6969, 6999)}.jpg"
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as resp:
-            if resp.status == 200:
-                if int(resp.headers['Content-Length']) > MAX_IMAGE_SIZE:
-                    return False
-                f = await aiofiles.open(file_name, mode='wb')
-                await f.write(await resp.read())
-                await f.close()
-            else:
+            if resp.status != 200:
                 return False
+
+            if int(resp.headers['Content-Length']) > MAX_IMAGE_SIZE:
+                return False
+            f = await aiofiles.open(file_name, mode='wb')
+            await f.write(await resp.read())
+            await f.close()
+            img = Image.open(file_name)
+            img.thumbnail((224, 224), Image.ANTIALIAS)
+            img.save(file_name)
+
     return file_name
